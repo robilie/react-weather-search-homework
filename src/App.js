5b076cc9acd04771fe6fa448eb304e310b880331
robilie <obilie.roberta@gmail.com>
2021-11-13 14:14:24 -0800

    added github link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <footer>
+        > This project was coded by Roberta Obilie and is{" "}
+        <a href="https://github.com/robilie/react-weather-search-homework.git">
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
