e6a2f7c3db3be3d843ff789141821ace9986e6c2
robilie <obilie.roberta@gmail.com>
2021-11-13 22:37:27 -0800

    Added API Integration

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="White Rock" />
         <footer>
           This project was coded by Roberta Obilie and is{" "}
           <a
--- src/Weather.js
+++ src/Weather.js
@@ -1,116 +1,148 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <div id="recBox">
-        <div className="row">
-          <div className="col-sm-4 daily-temp">
-            <h1>21°C</h1>
-          </div>
-          <div className="col-sm-4 daily-temp">
-            <div className="high-low">
-              <sup id="high">25</sup>/<sub id="low">18</sub>
-            </div>
-            <div className="alter-ego">
-              <sup>
-                <a href="/" id="celsius-link" className="temp-link">
-                  °C
-                </a>{" "}
-                |
-                <a href="/" id="fahrenheit-link" className="temp-link">
-                  °F
-                </a>
-              </sup>
-            </div>
-          </div>
-          <div className="col-sm-4 daily-temp">
-            <img
-              id="weather-image-today"
-              src="https://www.freeiconspng.com/uploads/cloud-sun-weather-icon-32.png"
-              className="weather-size"
-              alt=""
-            />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-6">
-            <h6 className="date-time">Mon October 11, 2:00pm</h6>
-          </div>
-          <div className="col-6">
-            <h6 className="feels-like" id="feels-like-temp">
-              Feels like 25°
-            </h6>
-          </div>
-        </div>
-        <h2>White Rock</h2>
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
-        <h3>Partly cloudy skies</h3>
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      description: response.data.weather[0].description,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      maxTemp: response.data.main.temp_max,
+      minTemp: response.data.main.temp_min,
+      feelsLike: response.data.main.feels_like,
+      date: "Mon October 11, 2:00pm",
+    });
+  }
 
-        <br />
-        <h4>DETAIL</h4>
-        <div className="row">
-          <div className="col">
-            <div className="card">
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/219/219816.png"
-                class="card-img-top"
-                alt="humidity"
-              />
-              <div className="card-body">
-                <p className="card-text">
-                  Humidity
-                  <br />
-                  <span id="humidity">42 </span>%
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="col">
-            <div className="card">
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/959/959711.png"
-                className="card-img-top"
-                alt="wind"
-              />
-              <div className="card-body">
-                <p className="card-text">
-                  Wind
-                  <br />
-                  <span id="wind-speed"> 1</span>
-                  <sub>km/hr </sub>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <br />
-
-        <form id="search-form">
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div id="recBox">
           <div className="row">
-            <div className="col-9">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter the City"
-                autoFocus="on"
-                id="search-text-input"
+            <div className="col-sm-4 daily-temp">
+              <h1>{Math.round(weatherData.temperature)}°C</h1>
+            </div>
+            <div className="col-sm-4 daily-temp">
+              <div className="high-low">
+                <sup id="high">{Math.round(weatherData.maxTemp)}</sup>/
+                <sub id="low">{Math.round(weatherData.minTemp)}</sub>
+              </div>
+              <div className="alter-ego">
+                <sup>
+                  <a href="/" id="celsius-link" className="temp-link">
+                    °C
+                  </a>{" "}
+                  |
+                  <a href="/" id="fahrenheit-link" className="temp-link">
+                    °F
+                  </a>
+                </sup>
+              </div>
+            </div>
+            <div className="col-sm-4 daily-temp">
+              <img
+                id="weather-image-today"
+                src={weatherData.icon}
+                className="weather-size"
+                alt=""
               />
             </div>
-            <div className="col-3">
-              <button
-                type="submit"
-                className="btn btn-light"
-                id="submit-city-button"
-              >
-                Search
-              </button>
+          </div>
+          <div className="row">
+            <div className="col-6">
+              <h6 className="date-time">{weatherData.date}</h6>
+            </div>
+            <div className="col-6">
+              <h6 className="feels-like" id="feels-like-temp">
+                Feels Like {Math.round(weatherData.feelsLike)}°
+              </h6>
             </div>
           </div>
-        </form>
+          <h2>{weatherData.city}</h2>
+
+          <h3>{weatherData.description}</h3>
+
+          <br />
+          <h4>DETAIL</h4>
+          <div className="row">
+            <div className="col">
+              <div className="card">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/219/219816.png"
+                  class="card-img-top"
+                  alt="humidity"
+                />
+                <div className="card-body">
+                  <p className="card-text">
+                    Humidity
+                    <br />
+                    <span id="humidity">
+                      {Math.round(weatherData.humidity)}{" "}
+                    </span>
+                    %
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col">
+              <div className="card">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/959/959711.png"
+                  className="card-img-top"
+                  alt="wind"
+                />
+                <div className="card-body">
+                  <p className="card-text">
+                    Wind
+                    <br />
+                    <span id="wind-speed"> {Math.round(weatherData.wind)}</span>
+                    <sub>km/hr </sub>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br />
+
+          <form id="search-form">
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter the City"
+                  autoFocus="on"
+                  id="search-text-input"
+                />
+              </div>
+              <div className="col-3">
+                <button
+                  type="submit"
+                  className="btn btn-light"
+                  id="submit-city-button"
+                >
+                  Search
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "f3e9b7fb8cbac59f9b2f8b3d635b8d32";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "The app is loading...";
+  }
 }
