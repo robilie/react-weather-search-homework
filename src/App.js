366fbbb6be36220babafb4925b9cc7f2fb1b9a4c
robilie <obilie.roberta@gmail.com>
2021-11-13 20:33:12 -0800

    added styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <footer>
-        This project was coded by Roberta Obilie and is{" "}
-        <a href="https://github.com/robilie/react-weather-search-homework.git">
-          open-sourced on github
-        </a>
-      </footer>
-      <button className="btn btn-primary">Hello</button>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by Roberta Obilie and is{" "}
+          <a
+            href="https://github.com/robilie/react-weather-search-homework.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
